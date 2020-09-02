5f7c1738e4069a82b0a232ce0d9a8c38887722b3
czv6803892 <835169097@qq.com>
2020-09-02 21:13:19 +0800

    fix(post-design): 修改topbar样式

--- packages/post-desgin/components/body-content.tsx
+++ packages/post-desgin/components/body-content.tsx
@@ -1,28 +1,20 @@
 import { Vue, Component } from 'vue-property-decorator';
-import { Drawer } from '@idg/iview';
 @Component({
   depends: [],
 })
 export default class BodyContent extends Vue {
-  public leftMenuShow: boolean = true;
   public rightMenuShow: boolean = true;
   public data() {
     return {};
   }
   public render() {
     return (
-      <div class='flex '>
-        <Drawer placement='left' v-model={this.leftMenuShow} mask={false} transfer={false}>
-          左侧抽屉
-        </Drawer>
-        <div class='p-15'>
-          <div class='bg-gray-0' style='width:80%;height:80%'>
-            22
-          </div>
-        </div>
-        <Drawer placement='right' v-model={this.rightMenuShow} mask={false} transfer={false}>
-          右侧抽屉
-        </Drawer>
+      <div class='flex w-full'>
+        <div class=''>我是左边菜单</div>
+
+        <div class='flex-grow'>我是中间内容</div>
+
+        <div class=''>我是右边菜单</div>
       </div>
     );
   }
--- packages/post-desgin/pages/page-lay-out.tsx
+++ packages/post-desgin/pages/page-lay-out.tsx
@@ -1,5 +1,5 @@
 import { Vue, Component } from 'vue-property-decorator';
-import { Layout, Footer, Button, Icon } from '@idg/iview';
+import { Footer, Button, Icon } from '@idg/iview';
 import style from '../styles/post-desgin.module.less';
 @Component({
   depends: ['component.BodyContent'],
@@ -10,27 +10,24 @@ export default class PageLayOut extends Vue {
   }
   public render() {
     return (
-      <div>
-        <Layout>
-          <div
-            class='t-0 absoulte w-full bg-gray-8 h-12 items-center flex text-lg flex justify-between'
-            style='z-index:2000'
-          >
+      <div class='h-screen'>
+        <div class={style.topBar}>
+          <div class={style.topBarWrap}>
             <div class={style.topBarBack}>
               <Icon type='ios-arrow-back' size={36} />
             </div>
-            <div class={style.topBarTitle}>postMaker</div>
+            <div class={style.topBarTitle}>postDesign</div>
             <Button type='primary' class='mr-4 items-center'>
               <Icon type='md-camera' class='mr-2' />保 存
             </Button>
           </div>
-          <div class='w-screen h-screen bg-gray-2'>
-            <body-content />
-          </div>
-          <Footer style='text-align:center' class='bg-gray-2'>
-            idg-post-maker@ 2020 Made By Billy-S
-          </Footer>
-        </Layout>
+        </div>
+        <div class='h-full'>
+          <body-content />
+        </div>
+        <Footer style='text-align:center' class='bg-gray-2'>
+          idg-post-maker@ 2020 Made By Billy-S
+        </Footer>
       </div>
     );
   }
